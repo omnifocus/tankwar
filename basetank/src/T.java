8b903b6a8a1cd741a9040c1c12918aa6b692e1b5
love <omnifocus@protonmail.com>
2021-08-07 22:23:59 +0800

    初始一个很简单的Frame

---
+++ basetank/src/T.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setTitle("tank war");
+        f.setSize(800,600);
+        f.setResizable(false);
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
